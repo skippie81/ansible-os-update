aadbd1d4fa972f7ce40a4f2b8c9703c57b128f79
skippie <joeri@scapicchio.be>
2017-11-24 12:52:21 +0100

    wget when no curl

--- scripts/get_latest_smartos_release.sh
+++ scripts/get_latest_smartos_release.sh
@@ -7,6 +7,16 @@ then
   URL='https://pkgsrc.joyent.com/packages/SmartOS/'
 fi
 
-RELEASE=$(curl -s $URL | egrep '20[0-9]{2}Q[0-9]{1}' | tail -1 | awk -F '[<>]' '{print $3}' | tr -d '/')
+if ( type curl 1> /dev/null 2>&1 )
+then
+  GET='curl -s'
+elif ( type wget 1> /dev/null 2>&1 )
+then
+  GET='wget -qO-'
+else
+  exit 1
+fi
+
+RELEASE=$($GET -s $URL | egrep '20[0-9]{2}Q[0-9]{1}' | tail -1 | awk -F '[<>]' '{print $3}' | tr -d '/')
 
 printf "$RELEASE"
